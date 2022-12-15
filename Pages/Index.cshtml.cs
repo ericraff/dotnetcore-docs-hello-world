2ce379fb43685b20477b80d8a9380c8c14545f8b
ericraff <eric.raff@outlook.com>
2022-12-15 14:42:35 -0700

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Runtime.InteropServices;
-using System.Secuirity.Claims;
+using System.Security.Claims;
 
 namespace dotnetcoresample.Pages;
 
