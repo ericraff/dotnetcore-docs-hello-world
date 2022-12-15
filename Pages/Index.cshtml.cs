00c4b38862ce33afc7679840e3de3f2f30637be6
ericraff <eric.raff@outlook.com>
2022-12-15 14:50:12 -0700

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,27 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Runtime.InteropServices;
 using System.Security.Claims;
 
+ 
+
 namespace dotnetcoresample.Pages;
+
+ 
 
 public class IndexModel : PageModel
 {
-
+    public string Message { get; set; } = "Initial value";
     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    
+
     private readonly ILogger<IndexModel> _logger;
+
+ 
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
 
+ 
+
     public void OnGet()
     {        
     ClaimsPrincipal cp = ClaimsPrincipal.Current;
     string userName = cp.FindFirst(ClaimTypes.WindowsAccountName).Value;
-    ViewBag.Message = String.Format("Hello {0}!", userName);
-    return View();
+    Message = String.Format("Hello {0}!", userName);
+
     }
 }
